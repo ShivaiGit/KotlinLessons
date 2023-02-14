310f1a6a98fb2a344d1820d56ac175ec2e0eae28
Nurulla Osmanov <Shivaii@yandex.ru>
2023-02-14 15:32:15 +0300

    Добавить кнопку в разметку и повесил на неё clickListener в Activity, при нажатии выходит всплывающее сообщение

--- app/src/main/java/ru/osmanov/kotlinlessons/MainActivity.kt
+++ app/src/main/java/ru/osmanov/kotlinlessons/MainActivity.kt
@@ -1,11 +1,22 @@
 package ru.osmanov.kotlinlessons
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.bnt_firstButton)
+
+        button.setOnClickListener {
+
+            val toast = Toast.makeText(applicationContext, "Кнопка работает", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
